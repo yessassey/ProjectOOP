17ab9bdadb178737fcd2c0f2dc0977912c102f7f
Nida Nur Hafizhah <fadasyi2018@gmail.com>
2025-12-11 12:02:17 +0700

    update file

--- PhonebookAppTrial/Person.cpp
+++ PhonebookAppTrial/Person.cpp
@@ -1,25 +1,26 @@
 #include "Person.h"
+using namespace std;
 
-Person::Person(std::string n, std::string nn, std::string p, std::string e, std::string a)
+Person::Person(string n, string nn, string p, string e, string a)
     : name(n), nickname(nn), phone(p), email(e), address(a) {}
 
-void Person::setName(const std::string &n) { name = n; }
-void Person::setNickname(const std::string &nn) { nickname = nn; }
-void Person::setPhone(const std::string &p) { phone = p; }
-void Person::setEmail(const std::string &e) { email = e; }
-void Person::setAddress(const std::string &a) { address = a; }
+void Person::setName(const string &n) { name = n; }
+void Person::setNickname(const string &nn) { nickname = nn; }
+void Person::setPhone(const string &p) { phone = p; }
+void Person::setEmail(const string &e) { email = e; }
+void Person::setAddress(const string &a) { address = a; }
 
-std::string Person::getName() const { return name; }
-std::string Person::getNickname() const { return nickname; }
-std::string Person::getPhone() const { return phone; }
-std::string Person::getEmail() const { return email; }
-std::string Person::getAddress() const { return address; }
+string Person::getName() const { return name; }
+string Person::getNickname() const { return nickname; }
+string Person::getPhone() const { return phone; }
+string Person::getEmail() const { return email; }
+string Person::getAddress() const { return address; }
 
 void Person::print() const
 {
-    std::cout << "Name: " << name
-              << "\nNickname: " << nickname
-              << "\nPhone: " << phone
-              << "\nEmail: " << email
-              << "\nAddress: " << address << "\n";
+    cout << "Name: " << name
+         << "\nNickname: " << nickname
+         << "\nPhone: " << phone
+         << "\nEmail: " << email
+         << "\nAddress: " << address << "\n";
 }
